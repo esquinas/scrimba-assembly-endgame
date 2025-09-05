1c451d5a6af4312f43b5dca2ca03cd4b2476ccc9
Enrique Esquinas <enrique.esquinas@marsbased.com>
2025-09-05 21:43:06 +0200

    Add word into separate character boxes
    /**
     * Goal: Build out the main parts of our app
     *
     * Challenge:
     * 1. Save a "currentWord" in state. Initialize as "react".
     * 2. Map over the letters of the word (you'll need to turn
     *    the string into an array of letters first) and display
     *    each one as a <span>. Capitalize the letters when
     *    displaying them.
     * 3. Style to look like the design. You can get the underline
     *    effect on the box using `border-bottom`.
     */

--- App.jsx
+++ App.jsx
@@ -3,12 +3,16 @@ import Chip from "./Chip"
 import { languages } from "./languages"
 
 export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = React.useState("react")
   const languageChips = languages.map(lang => (
     <Chip key={lang.name}
       name={lang.name}
       color={lang.color}
       backgroundColor={lang.backgroundColor}
     />
+  ))
+  const letterElements = currentWord.split("").map((letter, index) => (
+    <span className="char-box" key={letter + index}>{ letter }</span>
   ))
 
   return (
@@ -17,12 +21,18 @@ export default function AssemblyEndgame() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+
       <section className="game-status">
         <h3>You win!</h3>
         <p>Well done! 🎉</p>
       </section>
+
       <section className="language-chips">
         { languageChips }
+      </section>
+
+      <section className="word">
+        {letterElements }
       </section>
     </main>
   )
