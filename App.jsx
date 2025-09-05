a84b0215a9c9d74e67563971cd18f9357bbf0eb9
Enrique Esquinas <enrique.esquinas@marsbased.com>
2025-09-05 20:09:45 +0200

    Solution to Exercise 0

--- App.jsx
+++ App.jsx
@@ -1,16 +1,13 @@
 import React from "react"
 
-/**
- * Goal: Build out the main parts of our app
- *
- * Challenge: Add a header with the game title
- * and description. Startin' out easy 🙂🚶‍➡️
- */
 
-export default function Hangman() {
+export default function AssemblyEndgame() {
   return (
-    <main>
-      Game goes here
+    <main className="text-center">
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
     </main>
   )
 }
