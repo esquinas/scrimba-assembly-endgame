709a83ec9874f27c8794d3ec8472635f65741baa
Enrique Esquinas <enrique.esquinas@marsbased.com>
2025-10-16 18:41:36 +0200

    feat: add C2 level words

--- src/getRandomWord.js
+++ src/getRandomWord.js
@@ -1,4 +1,4 @@
-import { words } from "./data/words"
+import words from "./data/words.json"
 
 export default function getRandomWord() {
   const randomIndex = Math.floor(Math.random() * words.length);
